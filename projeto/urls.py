c20cc6f587cb676d6406202635b0547ea33638c4
Arnoldo Benther <abenther@gmail.com>
2023-12-07 22:40:48 -0300

    url

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def sobre(request):
+    return HttpResponse("String")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', sobre)
 ]
